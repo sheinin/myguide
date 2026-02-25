bd7e33c34c7c1abf2b0cd3e2fd901ce541c05a68
Sergei Sheinin <sergei.sheinin@gmail.com>
2026-02-25 17:50:15 +0700

    App

--- docs/products/run.sh
+++ docs/products/run.sh
@@ -1,29 +1,39 @@
 #!/bin/bash
 
 # Loop through all items in the current directory
+cd ../../../GroceryStoreDataset/dataset/pics
 declare -i count
-for itm in pics/*; do
+for itm in ./*; do
  for item in $itm/*; do
   # Check if the item is a directory
   if [ -d "$item" ]; then
     #echo "Processing directory: $item"
     for it in "$item"/*; do
      if [ -d "$it" ]; then
-       ((count += 1))
        f=$(ls $it/*.jpg)
-       #echo $f
        output=$(echo "${f##*/}" | tr '[:upper:]' '[:lower:]' | sed 's/[^a-z0-9_.]/_/g' | sed 's/^[0-9]/_/')
-       cp $it/*.jpg ./pic/$output
-       echo ${output%.*}
-       echo "###"
-       cat $it/*escription.txt
-       echo "###"
-       cat $it/*formation.txt
-       echo "***"
+       echo -e "${itm##*/}\t${item##*/}\t${it##*/}\t${output%.*}#$(cat $it/*escription.txt)|$(cat $it/*formation.txt)"
+       
+       #cp $it/*.jpg ./pic/$output
+     #  echo ${output%.*}
+     #  echo "###"
+     #  cat $it/*escription.txt
+     #  echo "###"
+     #  cat $it/*formation.txt
+      echo "***"
+     else
+#       echo -e "${itm##*/}\t${item##*/}\t${it##*/}"
+       f=$(ls $item/*.jpg)
+       output=$(echo "${f##*/}" | tr '[:upper:]' '[:lower:]' | sed 's/[^a-z0-9_.]/_/g' | sed 's/^[0-9]/_/')
+       echo -e "${itm##*/}\t${item##*/}\t${output%.*}#$(cat $item/*escription.txt)|$(cat $item/*formation.txt)"
+            echo "***"
      fi
     done
     # Perform operations on the directory here
     # Example: cd "$item" && run_command && cd ..
+   # else
+   #   echo -e "${itm}\t${item}\t${it}"
+   #   echo "*"
   fi
  done
 done
